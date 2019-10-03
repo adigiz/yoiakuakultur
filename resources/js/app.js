2e75f46dd9d239aad8f07da741e35b2ec3297b28
angga <anggakawa@gmail.com>
2019-10-03 15:26:28 +0800

    deleted comment on app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ require('./bootstrap');
 import Vue from 'vue';
 import CardComponent from './components/CardComponent';
 
-// components list
 Vue.component('card-component', CardComponent);
 
 const app = new Vue({
