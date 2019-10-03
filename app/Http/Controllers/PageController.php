301fa6f0161effb0208ce3cc5e3b2d799ef25ba0
angga <anggakawa@gmail.com>
2019-10-03 15:21:44 +0800

    deleting comment on PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,7 +11,6 @@ class PageController extends Controller
 {
     public function index() {
         $card_contents  = \App\CardContent::all();
-        // return $card_contents;
         return view('welcome', ['card_contents' => $card_contents]);
     }
 
